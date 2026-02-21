41ee5e4568c1abef9fa1a209f77640798f9589c0
Santiago Wilches <santiagowilchesv@gmail.com>
2026-02-20 21:28:41 -0500

    Add changes

---
+++ parallel_agent/cloud_run_deploy.sh
@@ -0,0 +1,7 @@
+adk deploy cloud_run \
+    --project=main-analog-487920-c9 \
+    --region=us-central1 \
+    --service_name=content-creator-agent \
+    --app_name=content-creator-agent \
+    --with_ui \
+    ./parallel_agent
